8ba9fdfebc8062ab8345a31ff5bb826465c8fe89
pavana-kuruvila <pavana.kuruvila@gmail.com>
2024-03-10 20:21:46 -0400

    commented code

--- dataClean.py
+++ dataClean.py
@@ -7,9 +7,11 @@ from torch.utils.data import DataLoader , random_split
 import matplotlib.pyplot as plt
 import torchvision.transforms.functional as TF
 
+#dataset path
 datasetPath = "/Users/vanakuruvila/Documents/School/COMP 472/DataSet2"
 
-#Data Cleaning 
+#---------  Data Cleaning transformations to apply ----------#
+
 data_transforms = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.RandomHorizontalFlip(0.5),
@@ -23,11 +25,14 @@ data_transforms = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # Standard from ImageNet
 ])
 
+#transforming the data
 dataset = datasets.ImageFolder(datasetPath, transform=data_transforms)
 print("Dataset classes: ", dataset.classes)
+#----------------------------------------------------------------#
 
 
-#Data Visualization: Bar Graph
+
+# ----------------- Data Visualization: Bar Graph -------------------- #
 distrubutionList = []
 
 classHappy = [(image, label) for image, label in dataset if dataset.classes[label] == dataset.classes[0]]
@@ -49,6 +54,8 @@ plt.ylabel('Number of Images in each Class')  # labling y-axis
 plt.xlabel('Class Name')           # labling x-axis
 plt.title('Distribution of images per class')  
 plt.show()   
+# ----------------- Data Visualization: Bar Graph -------------------- #
+
 
 
 #Split into traing and test sets
@@ -58,37 +65,46 @@ trainLoader = DataLoader(trainSet, shuffle=True, batch_size=32)
 testLoadeer = DataLoader(testSet, shuffle=True, batch_size=32)
 
 
-#Data Visuilization: Random images display 
+
+# ------------------ Data Visuilization: Random images display -------------------- #
 for i in range(4):
     samples = [(image, label) for image, label in trainSet if dataset.classes[label] == dataset.classes[i]][:25]
 
+    #displaying the transformed images in a 5x5 grid
     fig, axes = plt.subplots(5, 5, figsize=(12,8))
     for j, (image, label) in enumerate(samples):
+        
+        #transform images to PIL and display them in their grid spots
         pilImage = TF.to_pil_image(image)
         ax = axes[j // 5, j % 5]
         ax.imshow(pilImage)
         ax.set_title(f"Image #{j+1}")
         ax.axis("off")
 
+    #formatting and title
     plt.subplots_adjust(wspace=-0.7, hspace=0.5)
     plt.suptitle(f"Class: {dataset.classes[i]}", fontsize=22, fontweight="bold")
     plt.show()
 
+    #displaying the image pixel densities 
     fig, axes = plt.subplots(5, 5, figsize=(12,10))
     for k, (image, label) in enumerate(samples):
         pilImage = TF.to_pil_image(image)
 
         normalized_array = np.array(pilImage)
 
+        #exatracting each RGB channel to overlay on the histogram
         channel1 = normalized_array[:, :, 0].flatten()
         channel2 =normalized_array[:, :, 1].flatten()
         channel3 =normalized_array[:, :, 2].flatten()
 
+        #overlaying the density channels
         ax = axes[k // 5, k % 5]
         ax.hist(channel1, bins=100, range=[0, 256], density=True, color='red', alpha=0.5, label='Red')
         ax.hist(channel2, bins=100, range=[0, 256], density=True, color='green', alpha=0.5, label='Green')
         ax.hist(channel3, bins=100, range=[0, 256], density=True, color='blue', alpha=0.5, label='Blue')
         
+        #titles and grid line for graphs
         ax.set_title(f"Image #{k+1}", fontsize=8)
         ax.axis("on")
         ax.tick_params(axis='both', labelsize=8) 
@@ -96,12 +112,5 @@ for i in range(4):
     plt.subplots_adjust(wspace=0.5, hspace=0.6)
     plt.suptitle(f"Class: {dataset.classes[i]}", fontsize=22, fontweight="bold")
     plt.show()
-
+# ------------------ Data Visuilization: Random images display -------------------- #
         
-"""
-
-https://www.geeksforgeeks.org/how-to-normalize-images-in-pytorch/
-https://medium.com/@sehjadkhoja0/title-exploring-and-analyzing-image-data-with-python-79a7f72f4d2b
-https://www.kaggle.com/code/sanikamal/data-visualization-using-matplotlib
-https://www.geeksforgeeks.org/how-to-rotate-an-image-by-an-angle-using-pytorch-in-python/
-https://openreview.net/pdf?id=HXz7Vcm3VgM"""
