a586c464e9ebfd3f476f576dc86772551d15e4ea
pavana-kuruvila <pavana.kuruvila@gmail.com>
2024-03-22 10:05:54 -0400

    CNN start

---
+++ cnn.py
@@ -0,0 +1,91 @@
+import torch
+import numpy as np
+from torchvision import transforms 
+from PIL import Image
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader , random_split
+import matplotlib.pyplot as plt
+import torchvision.transforms.functional as TF
+import torch.nn as nn
+
+#dataset path
+datasetPath = "./dataset2/"
+
+#---------  Data Cleaning transformations to apply ----------#
+
+data_transforms = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(0.5),
+    transforms.RandomRotation(degrees=(-10, 10)),
+    transforms.RandomResizedCrop(size=(224, 224), scale=(0.9, 1.1), ratio=(0.9, 1.1)),
+    transforms.ColorJitter(brightness=(0.5,1.5),contrast=(0.5,1.5),saturation=(0.5,1.5),hue=(-0.1,0.1)),
+    transforms.GaussianBlur(kernel_size=3, sigma=2),
+    transforms.RandomGrayscale(p=0.1),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # Standard from ImageNet
+])
+
+#transforming the data
+dataset = datasets.ImageFolder(datasetPath, transform=data_transforms)
+print("Dataset classes: ", dataset.classes)
+#----------------------------------------------------------------#
+
+
+
+#Split into traing and test sets
+trainSet, testSet = random_split(dataset, [0.8,0.2])
+
+trainLoader = DataLoader(trainSet, shuffle=True, batch_size=32)
+testLoadeer = DataLoader(testSet, shuffle=True, batch_size=32)
+
+num_epochs = 20
+num_classes = 4
+learning_rate = 0.001
+
+classes = dataset.classes
+
+
+
+class CNN(nn.Module):
+
+    def __init__(self):
+        super(CNN, self).__init()
+        self.conv_layer = nn.Sequential(
+
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(inplace=True),
+
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(inplace=True),
+            
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.fc_layer = nn.Sequential(
+            nn.Dropout(p=0.1),
+            nn.Linear(8 * 8 * 64, 1000),
+            nn.ReLU(inplace=True),
+            nn.Linear(1000, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.1),
+            nn.Linear(512, 10)
+        )
+    
+def forward(self, x):
+# conv layers
+    x = self.conv_layer(x)
+    # flatten
+    x = x.view(x.size(0), -1)
+    # fc layer
+    x = self.fc_layer(x)
+    return x
